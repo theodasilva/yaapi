e196dbca1d0421a26395299442fa3c5ced50b365
dasilvatheo69@gmail.com <dasilvatheo69@gmail.com>
2024-11-06 04:17:06 +0000

    Created GA4CreateProperty; Edited Default and settings

--- client_code/GA4CreateProperty/__init__.py
+++ client_code/GA4CreateProperty/__init__.py
@@ -1,19 +1,16 @@
-from ._anvil_designer import DefaultTemplate
+from ._anvil_designer import GA4CreatePropertyTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
 
 
-class Default(DefaultTemplate):
+class GA4CreateProperty(GA4CreatePropertyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-
-  def reset_links(self, **event_args):
-    self.contact_link.role = ''
-    self.about_us_link.role = ''
+    # Any code you write here will run before the form opens.
