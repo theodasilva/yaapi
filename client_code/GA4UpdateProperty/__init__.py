817b78037dd0804c9cfa01e349f5e6aedf05fee1
dasilvatheo69@gmail.com <dasilvatheo69@gmail.com>
2024-11-06 05:15:10 +0000

    Edited settings and GA4UpdateProperty

--- client_code/GA4UpdateProperty/__init__.py
+++ client_code/GA4UpdateProperty/__init__.py
@@ -14,5 +14,8 @@ class GA4UpdateProperty(GA4UpdatePropertyTemplate):
     self.init_components(**properties)
     self.layout.reset_links()
     self.layout.update_property_link.role = "selected"
+    anvil.google.auth.login(["https://www.googleapis.com/auth/analytics.edit"])
+    access_token = anvil.google.auth.get_user_access_token()
+    print(access_token)
 
     # Any code you write here will run before the form opens.
